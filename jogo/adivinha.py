ffc0fc6a60773f4b0b72cc4bfcf887b16cbf4ecd
Otiliano Junior <otiliano.jr@gmail.com>
2022-07-25 14:42:57 -0300

    att game

--- jogo/adivinha.py
+++ jogo/adivinha.py
@@ -1,20 +1,20 @@
-from curses import endwin
-from queue import Empty
 import random
+import os
+
 
 print("*********************************")
 print("Bem vindo ao jogo de adivinhação!")
 print("*********************************")
 
+cur = dir(os)
+tentativas = 1
 
 print("Será sorteado um número, entre o intervalo escolhido!")
+n1 = int(input("Digite o primeiro número: "))
 print("*********************************")
-n1_str = input("Digite o primeiro número: ")
+n2 = int(input("Digite o segundo número: "))
 print("*********************************")
-n2_str = input("Digite o segundo número: ")
-print("*********************************")
-n1 = int(n1_str)
-n2 = int(n2_str)
+
 if(n1 > n2):
     print("O número escolhido estará entre ", n2, "e", n1)
     numero_random = random.randint(n2, n1)
@@ -25,24 +25,22 @@ else:
     print("Os números não podem ser iguais")
 
 print("*********************************")
-tentativa = input("Digite sua tentativa: ")
-chute = int(tentativa)
-if(tentativa == ""):
-    print("Você não colocou nenhum número", "\n", "Fim do jogo")
+
+chute = int(input("Digite sua tentativa: "))
+print("*********************************")
+while (chute != numero_random):
+    tentativas += 1
+    print("Você errou, Está é a", tentativas,
+          "º tentativa", "\n", "Tente novamente")
+    chute = int(input("Digite sua palpite: "))
     print("*********************************")
-else:
-    while (chute != numero_random):
-        tentativa = input("Digite sua tentativa: ")
-        chute = int(tentativa)
-        if(tentativa == ""):
-            print("Você não colocou nenhum número", "\n", "Fim do jogo")
-    if(chute == numero_random):
-        print("Você chutou", chute, "e acertou o número gerado foi ",
-              numero_random, "Parabéns")
-        print("Fim do jogo")
+if(chute == numero_random):
+    print("Você chutou o numero", chute, "e acertou!!", "\n"
+          "o número gerado foi ", numero_random, "Parabéns")
+    if (tentativas == 1):
+        print("Você acertou de primeira")
     else:
-        print("Você errou o número gerado foi ",
-              numero_random, " E você chutou ", chute)
-        print("Fim do jogo!")
+        print("Só foram ", tentativas, "tentativas")
+    print("Fim do jogo")
 
 print("*********************************")
