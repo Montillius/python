f75788335f6064dc312b20e22e2cb2b8fc0d7836
Otiliano Junior <otiliano.jr@gmail.com>
2022-07-27 15:01:15 -0300

    att jogos

---
+++ Jogos/jogar.py
@@ -0,0 +1,27 @@
+import sys
+import forca
+import adivinha
+
+def escolhe_jogo():
+    print("*********************************")
+    print("******Ecolha o seu jogo**********")
+    print("*********************************")
+
+    print("Forca       - (1)\nAdivinhação - (2)")
+    print("*********************************")
+
+    jogo = int(input("Qual jogo? "))
+
+    if(jogo ==1):
+        print("Jogando forca")
+        forca.jogar()
+    elif(jogo ==2):
+        print("Jogando adivinhação")
+        adivinha.jogar()
+    else:
+        print("Valor invalido")
+        print("*********************************")
+        sys.exit()
+
+if(__name__ == "__main__"):
+    escolhe_jogo()
