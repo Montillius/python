10d8a5a77a304cd58f82c0b48df8ce6cda102593
Otiliano Junior <otiliano.jr@gmail.com>
2022-08-24 12:07:51 -0300

    att api

--- fastapi_crud/main.py
+++ fastapi_crud/main.py
@@ -1,88 +1,64 @@
-from fastapi import FastAPI
-
+from datetime import datetime
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import Optional
+from uuid import uuid4 as uuid
 
 app = FastAPI()
 
 # lista
 classificacoes = []
 
-# ROTA INICIAL 
+# Post model
+class Classificacao_model(BaseModel):
+    id: Optional[str]
+    posicao: str
+    time: str
+    created_at: datetime = datetime.now()
+
+
+# index
 @app.get('/')
 def index():
     return {'Bem Vindo!': 'Esta é minha primera API'}
 
 
+# vizualizar lista
+@app.get('/tabela')
+def get_tabela():
+    return classificacoes
 
 
+# vizualizar único
+@app.get('/tabela/{id_classificacao}')
+def get_classificacao(id_classificacao: str):
+    for classificacao in classificacoes:
+        if classificacao['id'] == id_classificacao:
+            return classificacao
+    raise HTTPException(status_code=404, detail='Id não encontrado')
 
+# cadastrar
+@app.post('/tabela/cadastrar')
+def save_classificacao(dados: Classificacao_model):
+    dados.id = str(uuid())
+    classificacoes.append(dados.dict())
+    return classificacoes[-1]
 
+# editar
+@app.put('/tabela/{id_classificacao}')
+def update_classificacao(id_classificacao: str, update_classificacao:Classificacao_model):
+    for index, classificacao in enumerate(classificacoes):
+        if classificacao['id'] == id_classificacao:
+            classificacoes[index]['posicao'] = update_classificacao.posicao
+            classificacoes[index]['time'] = update_classificacao.time
+            return{'Mensagem':'A classificação foi editada com sucesso'}
+    raise HTTPException(status_code=404, detail='Id não encontrado')
 
-
-# from fastapi import FastAPI, HTTPException
-# from pydantic import BaseModel
-# from typing import Optional, Text
-# from datetime import datetime
-# from uuid import uuid4 as uuid
-
-# app = FastAPI()
-
-# classificacoes = []
-
-
-# class Tabela_model(BaseModel):
-#     id: Optional[str]
-#     posicao: str
-#     time: str
-#     created_at: datetime =  datetime.now()
-
-
-# # index
-# @app.get('/')
-# def home():
-#     return {"Bem vindo"}
-
-
-# # vizualização geral
-# @app.get('/tabela')
-# def get_classificacao():
-#     return classificacoes
-
-
-# # vizualizar único
-# @app.get('/tabela/{id}')
-# def get_classificacao(id: str):
-#     for classificacao in classificacoes:
-#         if classificacao["id"] == id:
-#             return classificacao
-#     raise HTTPException(status_code=404, detail="Item não encontrado")
-
-
-# # cadastrar
-# @app.post('/tabela/cadastrar')
-# def save_classificacao(dados: Tabela_model):
-#     dados.id = str(uuid())
-#     classificacoes.append(dados.dict())
-#     return dados[-1]
-
-
-# # editar
-# @app.put('/tabela/{id}')
-# def update_classificacao(id: str, update_classificacao: Tabela_model):
-#     for index, classificacao in enumerate(classificacoes):
-#         if classificacao["id"] == id:
-#             classificacoes[index]["posicao"]= update_classificacao.dict()["posicao"]
-#             classificacoes[index]["time"]= update_classificacao.dict()["time"]
-#             return {"mensagen": "A postagem foi atualizada com sucesso"}
-#     raise HTTPException(status_code=404, detail="Item não encontrado")
-
-
-# # excluir
-# @app.delete('/tabela/{id}')
-# def delete_classificacao(id: str):
-#     for index, classificacao in enumerate(classificacoes):
-#         if classificacao["id"] == id:
-#             classificacoes.pop(index)
-#             return {"mensagen": "A postagem foi excluída com sucesso"}
-#     raise HTTPException(status_code=404, detail="Item não encontrado")
-
-
+# excluir
+@app.delete('/tabela/{id_classificacao}')
+def delete_classificacao(id_classificacao: str):
+        for index, classificacao in enumerate(classificacoes):
+                if classificacao['id'] == id_classificacao:
+                    classificacoes.pop(index)
+                    return{'Mensagem':'A classificação foi deletada com sucesso'}
+        raise HTTPException(status_code=404, detail='Id não encontrado')
