9619cd4295a202c3957ce68496d432f8faf6dd27
Otiliano Junior <otiliano.jr@gmail.com>
2022-08-30 16:02:07 -0300

    fix: conta

--- python_oo/conta.py
+++ python_oo/conta.py
@@ -15,3 +15,23 @@ class Conta:
 
     def sacar(self, valor):
         self.__saldo -= valor
+
+    # O self serve para chamar métodos ou atributos
+    def transferir(self, valor, destino):
+        self.sacar(valor)
+        destino.depositar(valor)
+        
+    def get_saldo(self):
+        return self.__saldo 
+    
+    def get_titular(self):
+        return self.__titular
+    
+    def set_titular(self, titular):
+        self.__titular = titular
+    
+    def get_limite(self):
+        return self.__limite
+    
+    def set_limite(self, limite):
+        self.__limite = limite
