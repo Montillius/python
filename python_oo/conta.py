d8add18ed88f259d318769f244e53f2204dc6aa3
Otiliano Junior <otiliano.jr@gmail.com>
2022-09-01 15:01:21 -0300

    feat: metodo sacar

--- python_oo/conta.py
+++ python_oo/conta.py
@@ -1,7 +1,7 @@
 class Conta:
 
     def __init__(self, numero, titular, saldo, limite):
-        print("Construindo objeto ... {}". format(self))
+        print("Construindo objeto ... {}".format(self))
         self.__numero = numero
         self.__titular = titular
         self.__saldo = saldo
@@ -13,32 +13,40 @@ class Conta:
     def depositar(self, valor):
         self.__saldo += valor
 
-    def pode_sacar(self):
-        pass
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel_a_sacar = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disponivel_a_sacar
 
     def sacar(self, valor):
-        if(self.pode_sacar(valor)):
+        if(self.__pode_sacar(valor)):
             self.__saldo -= valor
         else:
-            print('O valor {} passou o limte'.format(valor))
+            print("O valor {} passou o limite".format(valor))
 
-    # O self serve para chamar métodos ou atributos
     def transferir(self, valor, destino):
         self.sacar(valor)
         destino.depositar(valor)
-        
+
     @property
     def saldo(self):
         return self.__saldo
-    
+
     @property
-    def titular(self):
+    def get_titular(self):
         return self.__titular
-    
+
     @property
     def limite(self):
         return self.__limite
-    
+
     @limite.setter
     def limite(self, limite):
         self.__limite = limite
+
+    @staticmethod
+    def codigo_banco():
+        return "001"
+
+    @staticmethod
+    def codigos_bancos():
+        return {'BB':'001', 'Caixa':'104', 'Bradesco':'237'}
